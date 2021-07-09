80313a67b7d5b72db0ae2966ba73cf18e2438f35
kosuke513 <kokun513@gmail.com>
2021-07-09 21:15:32 +0900

    add google chroam to test.yml

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |driver_options|
+    driver_options.add_argument('--disable-dev-shm-usage')
+    driver_options.add_argument('--no-sandbox')
+  end
 end
